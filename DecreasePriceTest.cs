292919f3ce89535583001960592e7e58694e3f7a
Jonathan Grehan-Butler <S00199080@mail.itsligo.ie>
2021-05-11 10:00:57 +0100

    Installed Entity Framework into the project

--- DecreasePriceTest.cs
+++ DecreasePriceTest.cs
@@ -11,20 +11,20 @@ namespace JonathanButler_S00199080
     [TestFixture]
     class DecreasePriceTest
     {
+        //This is my test that returns the incorrect result
         [Test]
-
         public static void Test()
         {
             Game z = new Game();
             Assert.AreEqual(1, z.DecreasePrice(5, 5));
         }
 
+        //This is my test that returns the correct result
         [Test]
-
         public static void Test1()
         {
             Game z = new Game();
-            Assert.AreEqual(0, z.DecreasePrice(5, 5));
+            Assert.AreEqual(0, z.DecreasePrice(5, 5)); //if the game is worth 5 and it is decreased by 5, it should return 0
         }
 
 
