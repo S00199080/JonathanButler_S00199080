c33ee101762b0ae1ea641ceb54ea3ab963fcf68f
Jonathan Grehan-Butler <S00199080@mail.itsligo.ie>
2021-05-11 09:49:58 +0100

    Created Unit Tests

---
+++ DecreasePriceTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JonathanButler_S00199080;
+using NUnit.Framework;
+
+namespace JonathanButler_S00199080
+{
+    [TestFixture]
+    class DecreasePriceTest
+    {
+        [Test]
+
+        public static void Test()
+        {
+            Game z = new Game();
+            Assert.AreEqual(1, z.DecreasePrice(5, 5));
+        }
+
+        [Test]
+
+        public static void Test1()
+        {
+            Game z = new Game();
+            Assert.AreEqual(0, z.DecreasePrice(5, 5));
+        }
+
+
+
+    }
+}
---
+++ Game.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JonathanButler_S00199080
+{
+    class Game
+    {
+        public string Name { get; set; }
+        public double Score { get; set; }
+        public string Description { get; set; }
+        public string Platform { get; set; }
+        public double Price { get; set; }
+        public string Game_Image { get; set; }
+
+        public double DecreasePrice(double discount, double price)
+        {
+            Price = price;
+
+            Price = price - discount;
+            return Price;
+        }
+
+    }
+}
